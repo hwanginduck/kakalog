7086cfac7d0ac8e290b0953f0fe24260447e6e6c
kaka7939 <kaka7939@kakao.com>
2022-11-24 17:56:22 +0900

    spring docs create

--- src/test/java/com/kakalog/controller/PostControllerDocTest.java
+++ src/test/java/com/kakalog/controller/PostControllerDocTest.java
@@ -1,14 +1,18 @@
 package com.kakalog.controller;
 
+import com.kakalog.domain.Post;
+import com.kakalog.repository.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -23,6 +27,9 @@ public class PostControllerDocTest {
 
     private MockMvc mockMvc;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @BeforeEach
     public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation){
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
@@ -31,11 +38,19 @@ public class PostControllerDocTest {
     }
 
     @Test
-    @DisplayName("1차 테스트")
+    @DisplayName("글 단건 조회 테스트")
     void test1234() throws Exception {
+        //given
+        Post post = Post.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+        postRepository.save(post);
+
         //expected
-        this.mockMvc.perform(get("/")
+        this.mockMvc.perform(get("/posts/{postId}", 1L)
                         .accept(MediaType.APPLICATION_JSON))
+                        .andDo(MockMvcResultHandlers.print())
                         .andExpect(status().isOk())
                         .andDo(document("index"));
     }
