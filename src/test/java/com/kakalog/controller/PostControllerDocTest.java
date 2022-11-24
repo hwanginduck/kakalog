5755bbc8adfd8f6d28473c601305d8ad5e4598c8
kaka7939 <kaka7939@kakao.com>
2022-11-25 08:26:34 +0900

    API 문서화

--- src/test/java/com/kakalog/controller/PostControllerDocTest.java
+++ src/test/java/com/kakalog/controller/PostControllerDocTest.java
@@ -1,41 +1,43 @@
 package com.kakalog.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kakalog.domain.Post;
 import com.kakalog.repository.PostRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.kakalog.request.PostCreate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.kakalog.com", uriPort = 443)
 @ExtendWith(RestDocumentationExtension.class)
 public class PostControllerDocTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private PostRepository postRepository;
+    private ObjectMapper objectMapper;
 
-    @BeforeEach
-    public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation){
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation))
-                .build();
-    }
+    @Autowired
+    private PostRepository postRepository;
 
     @Test
     @DisplayName("글 단건 조회 테스트")
@@ -49,9 +51,43 @@ public class PostControllerDocTest {
 
         //expected
         this.mockMvc.perform(get("/posts/{postId}", 1L)
-                        .accept(MediaType.APPLICATION_JSON))
-                        .andDo(MockMvcResultHandlers.print())
+                        .accept(APPLICATION_JSON))
+                        .andDo(print())
                         .andExpect(status().isOk())
-                        .andDo(document("index"));
+                        .andDo(document("post-inquiry",
+                                RequestDocumentation.pathParameters(
+                                        RequestDocumentation.parameterWithName("postId").description("게시글 ID")
+                                    ),
+                                responseFields(
+                                        fieldWithPath("id").description("게시글 ID"),
+                                        fieldWithPath("title").description("게시글 제목"),
+                                        fieldWithPath("content").description("게시글 내용")
+                                    )
+                                ));
+    }
+    @Test
+    @DisplayName("글 등록")
+    void test12345() throws Exception {
+        //given
+        PostCreate request = PostCreate.builder()
+                .title("나는 카카입니다.")
+                .content("글의 내용입니다.")
+                .build();
+        String json = objectMapper.writeValueAsString(request);
+
+        //expected
+        this.mockMvc.perform(post("/posts")
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON)
+                        .content(json))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("post-create",
+                        requestFields(
+                                fieldWithPath("title").description("게시글 제목")
+                                        .attributes(key("constraint").value("좋은제목을 입력해주세요.")),
+                                fieldWithPath("content").description("게시글 내용").optional()
+                        )
+                ));
     }
 }
